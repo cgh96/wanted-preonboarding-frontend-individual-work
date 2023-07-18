c60460fb51fe78910042a051c3f41cac0d862b6e
CGH <tt878429@gmail.com>
2023-07-18 12:34:47 +0900

    Feat: Sick API연동

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,16 +1,34 @@
 import "./SearchResult.scss";
 
 import SearchResultItem from "./SearchResultItem";
+import { useQueryStateContext } from "contexts/Query";
 
 function SearchResults() {
+  const { data, loading, error, isStale } = useQueryStateContext();
+
+  if (error) {
+    return 1;
+  }
+
+  if (loading) {
+    return 1;
+  }
+
+  if (isStale) {
+    return 1;
+  }
+
   return (
     <fieldset className="search-result-wrapper">
       <legend>추천 검색어</legend>
       <div style={{ height: "25px" }} aria-hidden />
+      {(!data || data?.length === 0) && "검색어 없음"}
       <ul>
-        <li>
-          <SearchResultItem />
-        </li>
+        {data?.map((e: any) => (
+          <li key={e.sickCd}>
+            <SearchResultItem sick={e} />
+          </li>
+        ))}
       </ul>
     </fieldset>
   );
--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -3,11 +3,17 @@ import "./SearchResultItem.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-function SearchResultItem() {
+import type { Sick } from "types/sick";
+
+interface SearchResultItemProps {
+  sick: Sick;
+}
+
+function SearchResultItem({ sick }: SearchResultItemProps) {
   return (
     <div className="search-result-item">
       <FontAwesomeIcon icon={faMagnifyingGlass} />
-      <span>Klatskin</span>
+      <span>{sick.sickNm}</span>
     </div>
   );
 }
